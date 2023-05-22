1a6dc338ae3ece7982a777ba74f9039edceea9be
Dennis Lynch <delynch@nvidia.com>
2023-05-22 15:54:30 -0700

    Advanced scattering snippets

---
+++ omni.replicator/snippets/scattering/multi_surface_min_max_scatter.py
@@ -0,0 +1,52 @@
+# SPDX-FileCopyrightText: Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES.
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# Copyright (c) 2022-2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     1. Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+#     2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+#     3. Neither the name of the copyright holder nor the names of its contributors
+# may be used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import omni.replicator.core as rep
+
+# Create a plane to scatter on
+plane_samp = rep.create.plane(scale=3, rotation=(20, 0, 0), visible=True)
+# Create a larger sphere to sample on the surface of
+sphere_samp = rep.create.sphere(scale=2.4, position=(0, 100, -200), visible=True)
+
+spheres = rep.create.sphere(scale=0.4, count=60)
+with spheres:
+    rep.randomizer.color(colors=rep.distribution.uniform((0, 0, 0), (1, 1, 1)))
+# randomize
+with rep.trigger.on_frame(1):
+    with spheres:
+        rep.randomizer.scatter_2d(
+            [plane_samp, sphere_samp],
+            min_samp=(None, -0.0001, -150),
+            max_samp=(None, 125, None),
+            check_for_collisions=False,
+        )
+
+# Menu: Replicator->Preview to view results
